ab3f3f6a5eb25397a00923e83a224cdcde1c730a
Ralph Neville-Jones <rbnevj@gmail.com>
2017-02-27 16:21:37 +0000

    added validations for table and price. Copied over and commented out the method which validates the durationand stops duration being set in the past the code can be used later.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :desk
+
+
+  validates :table, uniqueness: { scope: :user }
+  validates :price, presence: true
+
+#We will want to adapt this when we are further along.
+  # validate :duration_cannot_be_in_the_past
+  # def expiration_date_cannot_be_in_the_past
+  #   errors.add(:from, "can't be in the past") if
+  #     !from.blank? and from < Date.today
+  # end
 end
