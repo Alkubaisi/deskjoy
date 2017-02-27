be194c8df9ba5cb2603f6be02d85e2f654330272
Harry Bennett <harry.bennett1@gmail.com>
2017-02-27 16:09:38 +0000

    fix

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,5 +1,10 @@
 class Table < ApplicationRecord
   belongs_to :space
   has_many :users, through: :bookings
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :price, presence: true
+
 
 end
