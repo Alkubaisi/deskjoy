6d67ea35f74419ff0afc0efcb5a619dfd07e05d0
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-27 16:09:58 +0000

    validate space

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,11 +1,22 @@
 class Space < ApplicationRecord
   belongs_to :user
   has_many :desks, dependent: :destroy
-
-  validates :photo, presence: true
-
-
-
-
-
+  validate :name, presence: true
+  validate :address, presence: true
+  validate :industry, presence: true
+  validate :location, presence: true
+  validate :company_info, presence: true
+  validate :reciption, presence: true
+  validate :security, presence: true
+  validate :event_space, presence: true
+  validate :bike_storage, presence: true
+  validate :phone_booth, presence: true
+  validate :kitchen, presence: true
+  validate :lockers, presence: true
+  validate :cafe_restaurant, presence: true
+  validate :showers, presence: true
+  validate :meeting_room, presence: true
+  validate :hours, presence: true
+  validate :wifi, presence: true
+  validate :refreshment, presence: true
 end
