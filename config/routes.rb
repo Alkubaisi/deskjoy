036a15fce101b30225ae0d338d12eacfdaacbe09
Kit Tjia <wktjia@gmail.com>
2017-02-28 16:53:43 +0000

    basic show edit update profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
 
   devise_for :users
+  resources :profiles, only: [:show, :edit, :update]
+
   root to: 'pages#home'
-  get "/dashboard", to: "users#dashboard"
+  # get "/dashboard", to: "users#dashboard"
   get "/about", to: "pages#about"
   resources :spaces
     resources :tables, only: [:index, :show] do
