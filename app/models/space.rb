bd6403fd13787667e6168f58dc892cc221550d86
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-28 14:18:51 +0000

    done some changed to the form

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,23 +1,12 @@
 class Space < ApplicationRecord
-  has_attachment :photos, maximum: 4
+  has_attachments :photos, maximum: 4
   belongs_to :user
   has_many :desks, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
   validates :industry, presence: true
-  validates :location, presence: true
+
   validates :company_info, presence: true
-  validates :reciption, presence: true
-  validates :security, presence: true
-  validates :event_space, presence: true
-  validates :bike_storage, presence: true
-  validates :phone_booth, presence: true
-  validates :kitchen, presence: true
-  validates :lockers, presence: true
-  validates :cafe_restaurant, presence: true
-  validates :showers, presence: true
-  validates :meeting_room, presence: true
-  validates :hours, presence: true
-  validates :wifi, presence: true
-  validates :refreshment, presence: true
+    validates :photos, presence: true
+
 end
