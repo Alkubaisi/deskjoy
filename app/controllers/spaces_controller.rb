59e1b2ae6935ac1b20196f7985de26cf9a33ca85
Harry Bennett <harry.bennett1@gmail.com>
2017-03-01 10:26:29 +0000

    fixed controller bug spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,20 +1,20 @@
 class SpacesController < ApplicationController
+  before_action :set_space, only: [:show, :edit]
+
   def index
     @spaces = Space.all
+  end
+
+  def show
   end
 
   def new
     @space = Space.new
   end
-  
+
   def edit
-    set_space
   end
 
-  def show
-    set_space
-  end
-  
   def create
     @space = Space.new(space_params)
     @space.user = current_user
@@ -22,16 +22,15 @@ class SpacesController < ApplicationController
       redirect_to @space, notice: 'Space was successfully created.'
     else
       render :new
+    end
   end
 
   private
-    
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_space
     @space = Space.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def space_params
     params.require(:space).permit(:name, :address, :industry, :location, :company_info, :reciption, :security, :event_space, :bike_storage, :phone_booth, :kitchen, :lockers, :cafe_restaurant, :showers, :meeting_room, :hours, :wifi, :refreshment, photos: [])
   end
