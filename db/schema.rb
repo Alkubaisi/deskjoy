460ee881e9e0374aa7023e502b56f381d726e9af
fahad alkubaisi <alkubaisi9@gmail.com>
2017-03-02 16:22:03 +0000

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302084814) do
+ActiveRecord::Schema.define(version: 20170302150629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,9 @@ ActiveRecord::Schema.define(version: 20170302084814) do
     t.string   "linkedin"
     t.string   "twitter"
     t.string   "about"
-    t.string   "info"
+    t.text     "info"
+    t.string   "facebook"
+    t.string   "phonenumber"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
