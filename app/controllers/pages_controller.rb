a183f20c28efbaa2fbf9530cb9947dbc50f6409e
Harry Bennett <harry.bennett1@gmail.com>
2017-03-03 12:14:29 +0000

    homepage fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @spaces = Space.all.first(4)
+    @spaces = Space.all.sample(6)
   end
 
 
