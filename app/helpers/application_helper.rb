29b858c7d6d8f70905c001fece5c4b2abef947e4
fahad alkubaisi <alkubaisi9@gmail.com>
2017-03-05 14:18:24 +0000

    done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def avatar_for_user(user)
+    if user.photo?
+      cl_image_tag(user.photo.path, class: "card-avatar")
+    else
+      image_tag("avatar.png", class: "card-avatar")
+    end
+  end
+
+def avatar_for_user_show(user)
+    if user.photo?
+      cl_image_tag(user.photo.path, class: "avatar-bordered")
+    else
+      image_tag("avatar.png", class: "avatar-bordered")
+    end
+  end
+
+
 end
+
+
+
