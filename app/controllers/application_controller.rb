a958119d83e660a3b2d5d85a30a1b0f7271ada1b
Kit Tjia <wktjia@gmail.com>
2017-03-08 17:54:18 +0000

    meta tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def default_url_options
+    { Host: ENV [ 'HOST' ] ||  'Localhost: 3000' }
+  end
+
 end
+
+
